d77cc1fbded6e2ddd3129104e8e7bb16a6091676
esteban <112998820+Estebanbss@users.noreply.github.com>
2025-09-18 22:05:36 -0500

    Add base64 and sha1 dependencies; enhance WS-Security UsernameToken with PasswordDigest

--- src/ptz.rs
+++ src/ptz.rs
@@ -7,6 +7,11 @@ use axum::{
 };
 use quick_xml::events::Event;
 use quick_xml::Reader;
+use chrono::Utc;
+use rand::RngCore;
+use sha1::{Digest, Sha1};
+use base64::Engine as _;
+use base64::engine::general_purpose::STANDARD as BASE64;
 use std::sync::Arc;
 use url::Url;
 
@@ -17,30 +22,50 @@ const SOAP_ENV: &str = "http://www.w3.org/2003/05/soap-envelope"; // SOAP 1.2
 const NS_MEDIA: &str = "http://www.onvif.org/ver10/media/wsdl";
 const NS_PTZ: &str = "http://www.onvif.org/ver20/ptz/wsdl";
 const NS_WSSE: &str = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
-const NS_WSU_PWD_TEXT: &str = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText";
+const NS_WSU: &str = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+const NS_WSU_PWD_DIGEST: &str = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest";
+const NS_WSSE_NONCE_ENCODING: &str = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary";
 
 fn build_soap_envelope(username: &str, password: &str, inner_body: &str) -> String {
-    format!(
-        r#"<s:Envelope xmlns:s="{soap}" xmlns:wsse="{wsse}">
-  <s:Header>
-    <wsse:Security s:mustUnderstand="1">
-      <wsse:UsernameToken>
-        <wsse:Username>{user}</wsse:Username>
-        <wsse:Password Type="{pwd_type}">{pass}</wsse:Password>
-      </wsse:UsernameToken>
-    </wsse:Security>
-  </s:Header>
-  <s:Body>
-    {inner}
-  </s:Body>
+        // Build WS-Security UsernameToken with PasswordDigest as per ONVIF requirements
+        let mut nonce_bytes = [0u8; 16];
+        rand::thread_rng().fill_bytes(&mut nonce_bytes);
+        let nonce_b64 = BASE64.encode(nonce_bytes);
+        let created = Utc::now().format("%Y-%m-%dT%H:%M:%SZ").to_string();
+        let mut sha = Sha1::new();
+        sha.update(&nonce_bytes);
+        sha.update(created.as_bytes());
+        sha.update(password.as_bytes());
+        let digest = sha.finalize();
+        let digest_b64 = BASE64.encode(digest);
+
+        format!(
+                r#"<s:Envelope xmlns:s="{soap}" xmlns:wsse="{wsse}" xmlns:wsu="{wsu}">
+    <s:Header>
+        <wsse:Security s:mustUnderstand="1">
+            <wsse:UsernameToken>
+                <wsse:Username>{user}</wsse:Username>
+                <wsse:Password Type="{pwd_type}">{pwd}</wsse:Password>
+                <wsse:Nonce EncodingType="{nonce_enc}">{nonce}</wsse:Nonce>
+                <wsu:Created>{created}</wsu:Created>
+            </wsse:UsernameToken>
+        </wsse:Security>
+    </s:Header>
+    <s:Body>
+        {inner}
+    </s:Body>
 </s:Envelope>"#,
-        soap = SOAP_ENV,
-        wsse = NS_WSSE,
-        pwd_type = NS_WSU_PWD_TEXT,
-        user = xml_escape(username),
-        pass = xml_escape(password),
-        inner = inner_body
-    )
+                soap = SOAP_ENV,
+                wsse = NS_WSSE,
+                wsu = NS_WSU,
+                pwd_type = NS_WSU_PWD_DIGEST,
+                pwd = digest_b64,
+                nonce_enc = NS_WSSE_NONCE_ENCODING,
+                nonce = nonce_b64,
+                user = xml_escape(username),
+                created = created,
+                inner = inner_body
+        )
 }
 
 fn xml_escape(text: &str) -> String {
