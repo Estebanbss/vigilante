36eb6b5dd3dfdc721afd936228464af858001e26
teban <112998820+Estebanbss@users.noreply.github.com>
2025-10-02 14:35:41 -0500

    fix: Set payload type for H264 and Opus RTP payloader elements

--- src/stream/depends/webrtc.rs
+++ src/stream/depends/webrtc.rs
@@ -532,6 +532,7 @@ impl WebRTCManager {
         let rtph264pay = gst::ElementFactory::make("rtph264pay")
             .name("rtph264pay")
             .property("config-interval", 1i32)
+            .property("pt", 103i32)
             .build()?;
         let video_appsink = gst::ElementFactory::make("appsink")
             .name("video_appsink")
@@ -548,6 +549,7 @@ impl WebRTCManager {
             .build()?;
         let rtpopuspay = gst::ElementFactory::make("rtpopuspay")
             .name("rtpopuspay")
+            .property("pt", 111i32)
             .build()?;
         let audio_appsink = gst::ElementFactory::make("appsink")
             .name("audio_appsink")
